177c7e07ccca56a6f858a4ab428680b52fa03b39
MartinZhekov <martyj@mail.bg>
2020-11-09 21:10:35 +0200

    Added submodule to project and cofnigured pom dependencies

---
+++ core/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by martinzhekov on 9.11.20
+ */
+
+public class Main {
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        logger.info("Guess the number game");
+    }
+}
